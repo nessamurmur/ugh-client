a9403ff95c716209fb05ae270ed61343a2279098
Nate West <natescott.west@gmail.com>
2016-01-10 18:01:43 -0800

    Feature: start command

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ var version = require('../package.json').version
 var program = require('commander');
 var moment = require('moment');
 var nconf = require('nconf');
+var uuid = require('node-uuid');
 
 nconf.use('file', { file: process.env.HOME + '/.ughconfig.json' });
 nconf.load();
@@ -12,6 +13,14 @@ function configure(option, value) {
     console.log("Set " + option + " to " + value);
 };
 
+function start() {
+    var id = uuid.v4();
+    nconf.set('pending', id);
+    nconf.set('startedAt', new Date());
+    nconf.save();
+    console.log("Ugh... sorry this is painful...");
+}
+
 program
     .version(version);
 
@@ -19,5 +28,10 @@ program
     .command('configure <option> <value>')
     .description('Used to set config options for the ugh client')
     .action(configure);
+
+program
+    .command('start')
+    .description('Begins time tracking an issue')
+    .action(start);
 
 program.parse(process.argv);
