197e5899f9af554eae7d732f3952dbf0ad642064
Nate West <natescott.west@gmail.com>
2016-01-10 19:17:25 -0800

    Client works!

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ var program = require('commander');
 var moment = require('moment');
 var nconf = require('nconf');
 var uuid = require('node-uuid');
+var Bluebird = require('bluebird');
+var promptly = Bluebird.promisifyAll(require('promptly'));
+var request = require('request');
 
 nconf.use('file', { file: process.env.HOME + '/.ughconfig.json' });
 nconf.load();
@@ -16,9 +19,60 @@ function configure(option, value) {
 function start() {
     var id = uuid.v4();
     nconf.set('pending', id);
-    nconf.set('startedAt', new Date());
+    nconf.set('startedAt', moment());
     nconf.save();
     console.log("Ugh... sorry this is painful...");
+}
+
+function postToUgh(params, callback) {
+    request.post({
+        url: nconf.get('issuesUrl'),
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(params)
+    }, function(err, resp, body) {
+        if (!err) {
+            callback(JSON.parse(body).issue.id);
+        } else {
+            console.log(err);
+        }
+    });
+}
+
+function cleanup() {
+    nconf.clear('pending');
+    nconf.clear('startedAt');
+    nconf.save();
+}
+
+function promptText() {
+    return promptly.promptAsync('What was the issue?')
+}
+
+function promptRepo() {
+    return promptly.promptAsync('What is the link to this repo?')
+}
+
+function stop() {
+    if (!nconf.get('pending')) {
+        throw "You haven't started an issue!"
+    };
+    var startedAt = nconf.get('startedAt');
+    var timestamp = moment().subtract(startedAt).seconds();
+    var user = process.env.USER;
+    var text, repo;
+    promptText()
+        .then(function (answer) {
+            text = answer;
+        })
+        .then(promptRepo)
+        .then(function(answer) {
+            repo = answer;
+        })
+        .then(function() {
+            var params = {repo: repo, reporter: user, time_wasted: timestamp, text: text};
+            postToUgh(params, function(id) { console.log("Issue " + id + " has been filed!") });
+        })
+        .done(cleanup);
 }
 
 program
@@ -33,5 +87,10 @@ program
     .command('start')
     .description('Begins time tracking an issue')
     .action(start);
+
+program
+    .command('stop')
+    .description('Stop time tracking an issue and publish to ugh')
+    .action(stop);
 
 program.parse(process.argv);
